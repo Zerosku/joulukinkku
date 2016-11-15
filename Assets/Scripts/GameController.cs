6e0f0c84740d9cc09070f9e014fca8730ae6f98e
goebers <kack@cock.li>
2016-11-15 22:56:00 +0200

    changes to player control

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,8 +6,6 @@ public class GameController : MonoBehaviour {
 
 	private GameObject player;
 
-	// pelaajan nopeus
-	public float playerSpeed = 0.05f;
 
 	void Start () {
 		player = GameObject.Find ("testipelaaja"); // temporary shits
@@ -15,21 +13,6 @@ public class GameController : MonoBehaviour {
 
 	void Update () {
 		
-		// player movement
-		if (Input.GetKey("up")) {
-			Debug.Log ("Move up");
-			player.transform.Translate (0, playerSpeed, 0);
-			Debug.Log (playerSpeed);
-		} else if (Input.GetKey("down")) {
-			Debug.Log ("Move down");
-			player.transform.Translate (0, -playerSpeed, 0);
-		} else if (Input.GetKey("left")) {
-			Debug.Log ("Move Left");
-			player.transform.Translate (-playerSpeed, 0, 0);
-		} else if (Input.GetKey("right")) {
-			Debug.Log ("Move right");
-			player.transform.Translate (playerSpeed, 0, 0);
-		}
 	}
 
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,21 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+public class Player : GameController {
 
+	private GameObject player;
+
+	// pelaajan nopeus
+	public float playerSpeed = 0.03f;
 
 	// Use this for initialization
 	void Start () {
-		
+		player = GameObject.Find ("testipelaaja");
 	}
 
 	// Update is called once per frame
 	void Update () {
-		//Player.MovePlayer (playerSpeed);
+		Movement (playerSpeed);
 	}
-
-	void OnCollisionEnter2D (Collision2D col){
-		transform.Translate (0, 1.0f, 0); // puskee pelaajan johonkin osuessaan
+	// player movements
+	public void Movement (float playerSpeed){
+		if (Input.GetKey("up")) {
+			Debug.Log ("Move up");
+			player.transform.Translate (0, playerSpeed, 0);
+		}
+		else if (Input.GetKey("down")) {
+			Debug.Log ("Move down");
+			player.transform.Translate (0, -playerSpeed, 0);
+		}
+		else if (Input.GetKey("left")) {
+			Debug.Log ("Move left");
+			player.transform.Translate (-playerSpeed, 0, 0);
+		}
+		else if (Input.GetKey("right")) {
+			Debug.Log ("Move right");
+			player.transform.Translate (playerSpeed, 0, 0);
+		}
 	}
 		
 }
