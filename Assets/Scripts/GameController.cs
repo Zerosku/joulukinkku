6eced221a86789e5c666f4b3cb411f4eb11a6553
goebers <kack@cock.li>
2016-11-15 14:52:43 +0200

    added first basic player controlling

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	private GameObject player;
+
+	// pelaajan nopeus
+	public float playerSpeed = 0.05f;
+
+	void Start () {
+		player = GameObject.Find ("testipelaaja"); // temporary shits
+	}
+
+	void Update () {
+		
+		// player movement
+		if (Input.GetKey("up")) {
+			Debug.Log ("Move up");
+			player.transform.Translate (0, playerSpeed, 0);
+			Debug.Log (playerSpeed);
+		} else if (Input.GetKey("down")) {
+			Debug.Log ("Move down");
+			player.transform.Translate (0, -playerSpeed, 0);
+		} else if (Input.GetKey("left")) {
+			Debug.Log ("Move Left");
+			player.transform.Translate (-playerSpeed, 0, 0);
+		} else if (Input.GetKey("right")) {
+			Debug.Log ("Move right");
+			player.transform.Translate (playerSpeed, 0, 0);
+		}
+	}
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//Player.MovePlayer (playerSpeed);
+	}
+
+	void OnCollisionEnter2D (Collision2D col){
+		transform.Translate (0, 1.0f, 0); // puskee pelaajan johonkin osuessaan
+	}
+		
+}
