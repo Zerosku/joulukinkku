1d7ef436b7ce0a1b1f6fd13b634369461116b69e
goebers <kack@cock.li>
2016-11-18 10:19:22 +0200

    Unohin nää enemy scribit lisää

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : Player {
+
+	private static GameObject enemy;
+
+
+	// Use this for initialization
+	void Start () {
+		enemy = GameObject.Find ("Enemy");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//EnemyTrigger.EnemyAggro ();
+	}
+		
+}
---
+++ Assets/Scripts/EnemyTrigger.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyTrigger : Enemy {
+
+	private GameObject EnemyCollider;
+	private static bool touching = false;
+
+	private static GameObject enemy;
+
+	// vihollisen nopeus
+	public static float enemySpeed = 0.02f;
+
+	// vihun vector2
+	private static Vector2 enemyVector = enemy.transform.position;
+
+	// mitä kohti lähdetään EnemyAggro () : ssa
+	public static Vector2 playerMan = Player.player.transform.position;
+
+	private static Transform transform;
+
+	// Use this for initialization
+	void Start () {
+		enemy = GameObject.Find ("Enemy");
+		EnemyCollider = GameObject.Find ("EnemyTrigger");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		EnemyAggro ();
+	}
+		
+	// enemysettii
+
+	public static void EnemyAggro (){
+		if (touching){
+			Debug.Log ("enemy triggered");
+			//transform.position = Vector2.MoveTowards(enemyVector, playerMan, enemySpeed * Time.deltaTime);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Player") {
+			touching = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if (other.tag != "Player") {
+			touching = false;
+		}
+	}
+}
