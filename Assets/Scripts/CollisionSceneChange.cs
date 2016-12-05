2fca06933e289f71b8f206faab30af1239bf9bfd
Sara Imbert <saraimbert@MacBook-Air-de-Sara.local>
2016-12-05 10:23:48 +0200

    1. scene aloitettu, samoin scenenvaihto script

---
+++ Assets/Scripts/CollisionSceneChange.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CollisionSceneChange : MonoBehaviour {
+
+
+
+
+
+	void OnTriggerEnter2D (Collision2D collider){
+	
+		if (collider.gameObject.tag == "player") {
+			SceneManager.LoadScene (2);
+	
+		}
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
